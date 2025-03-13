aee3f2b9066ee57531dd18c23f71a842c5017ec6
VAMPIR4894 <aditya4894a@gmail.com>
2025-03-13 13:02:50 +0530

    Added task 2 description line

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  std::cout << "This is Task 2 for Jenkins Automation modified by Me PES1UG22CS035\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
